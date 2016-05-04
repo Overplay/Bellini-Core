b4798e17e47a46606c057c7b74cbcdc93ed48194
colegrigsby <cole.grigsby@gmail.com>
2016-05-04 14:21:11 -0700

    Updated to uib-directives - now dropdowns are quicker. Navbar now works in hamburger mode. Dropdown menus no longer close on mouseleave. Hamburger menu collapses when a link within is clicked (not done very DRYly though).

--- assets/uiapp/app/components/directives/navbar/navbar.directive.js
+++ assets/uiapp/app/components/directives/navbar/navbar.directive.js
@@ -1,19 +1,18 @@
-app.directive( 'navigationBar', function(){
+app.directive('navigationBar', function ($log) {
 
     return {
-        restrict:    "E",
+        restrict: "E",
         templateUrl: "/uiapp/app/components/directives/navbar/navbar.template.html",
         scope: {
             info: '='
         },
-        link:  function ( scope, element, attrs ) {
+        link: function (scope, element, attrs) {
 
             scope.ui = {
                 adminOpen: false,
-                showAdmin: attrs.adminMode!=undefined,
+                showAdmin: attrs.adminMode != undefined,
                 usersOpen: false
-                };
-            
+            };
 
         }
     }
