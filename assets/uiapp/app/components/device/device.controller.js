a017b99d7ad1fa75988d1f95d85b0a39a31d7f2d
colegrigsby <cole.grigsby@gmail.com>
2016-05-11 17:57:08 -0700

    activate device partial and controller for testing the registration codes

--- assets/uiapp/app/components/device/device.controller.js
+++ assets/uiapp/app/components/device/device.controller.js
@@ -9,10 +9,35 @@ app.controller("addDeviceController", function ($scope, $state, $log, toastr, nu
     $log.debug("addDeviceController starting.");
     $scope.device = {};
 
-    $http.post('/activation/generateCode', {})
+    $http.post('/activation/generateCode', $scope.device)
         .then(function (data) {
             $scope.data = data.data;
         });
 
-    
+
+});
+
+//THis controller will be something the device implements, i just created it for
+//testing purposes - coal 
+app.controller("registerDeviceController", function ($scope, $state, $log, toastr, nucleus, $http) {
+
+    $log.debug("registerDeviceController starting.");
+    $scope.device = {};
+
+    $scope.register = function () {
+        $log.log($scope.device);
+        $http.post('/device/registerDevice', $scope.device)
+            .then(function (data) {
+                toastr.success("Device Registered!", "Success!");
+
+                $state.go('admin.manageDevices');
+            })
+            .catch(function (err) {
+                toastr.error("Device activation code not found", "Damn!");
+                $scope.device.regCode = '';
+            });
+
+
+    };
+
 });
