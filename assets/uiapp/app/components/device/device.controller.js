e056c21dc25b47b60dc355d332bd091264baf625
colegrigsby <cole.grigsby@gmail.com>
2016-05-16 17:26:36 -0700

    Ui-app device management and registration

--- assets/uiapp/app/components/device/device.controller.js
+++ assets/uiapp/app/components/device/device.controller.js
@@ -4,46 +4,58 @@
 
 
 
-app.controller("addDeviceController", function ($scope, $state, $log, toastr, nucleus, $http) {
+app.controller("addDeviceController", function ($scope, $state, $log, toastr, nucleus, $http, user, uibHelper) {
 
     $log.debug("addDeviceController starting.");
     $scope.device = {};
+    $scope.user = user;
+    $scope.code = false;
 
-    $http.post('/activation/generateCode', $scope.device)
-        .then(function (data) {
-            $scope.data = data.data;
-        })
-        .catch(function (err) {
-            toastr.error("Device activation code not generated", "Damn!");
-        });
+    //$scope.device.venue = $scope.user.venue[0];
+
+    $scope.submitForCode = function () {
+        $http.post('/activation/generateCode', $scope.device)
+            .then(function (data) {
+                $scope.code = true;
+                $scope.data = data.data;
+            })
+            .catch(function (err) {
+                toastr.error("Device activation code not generated", "Damn!");
+            });
+    }
+
 
 
 });
 
 //THis controller will be something implemented on the device itself, i just created it for
 //testing purposes - cole
-app.controller("registerDeviceController", function ($scope, $state, $log, toastr, nucleus, $http) {
 
-    $log.debug("registerDeviceController starting.");
-    $scope.device = {};
+/*
+ app.controller("registerDeviceController", function ($scope, $state, $log, toastr, nucleus, $http) {
 
-    $scope.register = function () {
-        $log.log($scope.device);
-        $http.post('/device/registerDevice', $scope.device)
-            .then(function (data) {
-                toastr.success("Device Registered!", "Success!");
+ $log.debug("registerDeviceController starting.");
+ $scope.device = {};
 
-                $state.go('admin.manageDevices');
-            })
-            .catch(function (err) {
-                toastr.error("Device activation code not found", "Damn!");
-                $scope.device.regCode = '';
-            });
+ $scope.register = function () {
+ $log.log($scope.device);
+ $http.post('/device/registerDevice', $scope.device)
+ .then(function (data) {
+ toastr.success("Device Registered!", "Success!");
+
+ $state.go('admin.manageDevices');
+ })
+ .catch(function (err) {
+ toastr.error("Device activation code not found", "Damn!");
+ $scope.device.regCode = '';
+ });
 
 
-    };
+ };
 
-});
+ });
+
+ */
 
 app.controller("editDeviceAdminController", function ($scope, $state, $log, device, toastr, uibHelper, nucleus) {
 
@@ -59,6 +71,30 @@ app.controller("editDeviceAdminController", function ($scope, $state, $log, devi
             .catch(function (err) {
                 toastr.error("Something went wrong", "Damn!");
             });
+    }
+
+    $scope.deleteDevice = function () {
+
+        $log.log("Delete")
+        uibHelper.confirmModal("Delete Device?", "Are you sure you want to delete device " + $scope.device.name, true)
+            .then(function (confirmed) {
+                if (confirmed) { // probably not necessary since reject should be called for cancel
+
+                    nucleus.deleteDevice($scope.device)
+                        .then(function () {
+                            toastr.success("It's gone!", "Device Deleted");
+                            $state.go('admin.manageDevices')
+                        })
+                        .catch(function (err) {
+                            toastr.error(err.status, "Problem Deleting Device");
+                        })
+
+
+                }
+
+            })
+
+
     }
 
 
