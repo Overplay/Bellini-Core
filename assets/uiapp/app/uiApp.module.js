8c6bc7fed11df86a2ab0dd033c686957bfa1f60c
colegrigsby <cole.grigsby@gmail.com>
2016-07-29 10:56:12 -0700

    cleanup

--- assets/uiapp/app/uiApp.module.js
+++ assets/uiapp/app/uiApp.module.js
@@ -25,27 +25,8 @@ app.run( function ( $log, $rootScope ) {
             $log.error( "State change fail!" );
         } )
 
-   /* $rootScope.$on('$stateChangeStart',
-        function(event, toState, toParams, fromState, fromParams, options){
-            //TODO test user and toState and reroute if necessary
-                // maybe write service and inject into app.run above then call it here
-                //http://stackoverflow.com/questions/22537311/angular-ui-router-login-authentication
-                // prevent default if the user is not allowed to access a page
-                // worries - speed
-                // define somewhere? use existing navbar array links to define what users can access?
-                // issue example - Organization view/edit is one controller/route
-                    // there exists a view only though! - just gotta make sure routing is correct
-            // or http://stackoverflow.com/questions/28518181/angular-ui-router-how-to-prevent-access-to-a-state
 
-            $log.log("event " , event);
-            $log.log("toState " , toState);
-            $log.log("toParams " , toParams);
-            $log.log("fromState " , fromState);
-            $log.log("fromParams " , fromParams);
 
-            $log.log("options " , options);
-            //event.preventDefault(); to keep from changing to the new state
-        })*/
 
 } );
 
