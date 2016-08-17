e2c1a0a364b7797fe6f4f56a04c5a798da0d5d7a
colegrigsby <cole.grigsby@gmail.com>
2016-08-17 16:16:56 -0700

    playing around

--- test/testLogin.js
+++ test/testLogin.js
@@ -10,3 +10,10 @@ driver.get('http://localhost:1337/login');
 driver.findElement(By.name('email')).sendKeys('elizabeth@test.com');
 driver.findElement(By.name('password')).sendKeys('pa$$word');
 driver.findElement(By.id('login')).click();
+driver.wait(function(){
+    return driver.getCurrentUrl().then(function(url){
+        console.log(url)
+        return url == "http://localhost:1337/ui#/dash"
+    })
+}, 1000)
+driver.findElement(By.xpath("//*[contains(.,'Managers')]")).click()
