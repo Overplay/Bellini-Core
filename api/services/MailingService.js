38a1263274fddf4b49ef997e9577638d03a5757c
colegrigsby <cole.grigsby@gmail.com>
2016-09-02 11:38:33 -0700

    Fix path normalizing for newer versions of node

--- api/services/MailingService.js
+++ api/services/MailingService.js
@@ -95,9 +95,7 @@ module.exports = {
 }
 
 var sendMail = function (viewVars, template, subject, mailTo) {
-    var templatePath = path.normalize(__dirname + "../../../views/emails/" + template);
-    sails.log.debug(templatePath)
-    sails.log.debug(__dirname)
+    var templatePath = path.normalize(__dirname + "/../../views/emails/" + template);
     var html = jade.renderFile(templatePath, viewVars);
 
     var mailOptions = {
