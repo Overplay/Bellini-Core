f18892c3e3f6c870b456e5cabc37ef5c26b04df0
colegrigsby <cole.grigsby@gmail.com>
2016-09-07 15:21:57 -0700

    fix test generator

--- config/testdata.js
+++ config/testdata.js
@@ -9,7 +9,7 @@ var adName = 'Advertisement One!';
 var adDate = 'September 7'; //TODO
 var self = module.exports.testdata = {
 
-    installTestData: true,
+    installTestData: false,
     eraseOldData: false,
 
     install: function () {
