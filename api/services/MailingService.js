75291790f39f8b99121dd2011a1ad8e456388a6b
colegrigsby <cole.grigsby@gmail.com>
2016-09-12 08:50:16 -0700

    ad review mailing

--- api/services/MailingService.js
+++ api/services/MailingService.js
@@ -56,6 +56,7 @@ module.exports = {
                 sails.log.debug(us)
                 var emails = "";
                 us.forEach(function (u) {
+                    sails.log.debug(u)
                     emails += u.auth.email + ","
                 })
                 return emails;
