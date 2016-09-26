9679524ab3533c7fa420ae8e7764736c342fb059
colegrigsby <cole.grigsby@gmail.com>
2016-09-26 14:10:32 -0700

    ip check

--- api/policies/hasSameIP.js
+++ api/policies/hasSameIP.js
@@ -2,16 +2,12 @@
  * has Same req ip
  *
  * @module      :: Policy
- * @description :: checks if the request has an ip
+ * @description :: checks if the request has an ip that matches 
  *
  * @docs        :: http://waterlock.ninja/documentation
  */
 
-// TODO: Should be able to combine this with the other auth policies to create a single
-// Policy.
 
-
-//Waterlock methods to get user and stuff too so we should figure it out
 module.exports = function (req, res, next) {
 
 
@@ -20,10 +16,22 @@ module.exports = function (req, res, next) {
         return next();
     }
     else {*/
+    //maybe just change this to a config ip and check request ip
         require('dns').lookup(require('os').hostname(), function (err, add, fam) {
             sails.log.debug(add)
+            if (add.substr(0, 7) == "::ffff:") {
+                add = add.substr(7)
+            }
+            var ip = req.ip;
+            sails.log.debug(ip)
+            if (req.ip.substr(0, 7) == "::ffff:") { //dealing with ipv6 
+                ip = req.ip.substr(7)
+            }            
+            sails.log.debug(ip)
+
+            sails.log.debug(add == ip, req.headers.host, req.host)
         })
-        sails.log.debug(req.ip)
+    
     //}
 
     next();
