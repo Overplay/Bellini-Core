47666340b7c23916e46ab02cf95a08cbac91897a
Mitch Kahn <mitch@ourglass.io>
2018-02-01 11:18:11 -0800

    Adding support for `x-og-authorization` to get past Nginx wanting to fuck with JWTs.

--- api/policies/sessionAuth.js
+++ api/policies/sessionAuth.js
@@ -10,6 +10,11 @@
 
 module.exports = function ( req, res, next ) {
 
+    if (req.headers['x-og-authorization']){
+        sails.log.silly('Using x-og-authorization');
+        req.headers.authorization = req.headers[ 'x-og-authorization' ];
+    }
+
     if ( sails.config.policies.godToken && req.headers.authorization && req.headers.authorization === "Bearer OriginalOG") {
             next();
     } else if ( req.headers.authorization ){
