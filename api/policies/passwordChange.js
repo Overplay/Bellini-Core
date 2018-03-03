d88868ec1083af6eef475c777f9f8952c6535a42
Mitch Kahn <mitch@ourglass.io>
2018-03-03 11:26:57 -0800

    DEBUGGING: Change to policies for Android password change.

--- api/policies/passwordChange.js
+++ api/policies/passwordChange.js
@@ -25,7 +25,8 @@ module.exports = function (req, res, next) {
         return next();
     }
 
+    sails.log.silly('Falling out of passwordChange policy! User: ' + req.session.user );
     // User is not allowed
     // (default res.forbidden() behavior can be overridden in `config/403.js`)
-    return res.forbidden('You are not permitted to perform this action.');
+    return res.forbidden({ error: 'You are not permitted to perform this action.'});
 };
